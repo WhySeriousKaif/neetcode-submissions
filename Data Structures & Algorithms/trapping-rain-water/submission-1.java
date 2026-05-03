26a2d43ec0577668a81f5771eb3efe277bfd57ad
MD Kaif Molla <139871820+WhySeriousKaif@users.noreply.github.com>
2026-05-03 23:59:17 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int l = 0, r = height.length - 1;
+        int leftMax = height[l], rightMax = height[r];
+        int res = 0;
+        while (l < r) {
+            leftMax = Math.max(leftMax, height[l]);
+            rightMax = Math.max(rightMax, height[r]);
+            if (leftMax < rightMax) {
+                res += leftMax - height[l];
+                l++;
+            } else {
+                res += rightMax - height[r];
+                r--;
+            }
+        }
+        return res;
+    }
+}
