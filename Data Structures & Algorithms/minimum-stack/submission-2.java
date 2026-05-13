74eec63c939903f8e7deefbdb05a6be88eccb2ad
MD Kaif Molla <139871820+WhySeriousKaif@users.noreply.github.com>
2026-05-13 19:14:38 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,48 @@
+class MinStack {
+    Stack<Integer> st;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        st = new Stack<>();
+        minStack = new Stack<>();
+
+    }
+
+    public void push(int val) {
+        st.add(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.add(val);
+        }
+
+    }
+
+    public void pop() {
+        if (st.isEmpty())
+            return;
+
+        // Use .equals() for reliable object value comparison
+        if (st.pop().equals(minStack.peek())) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return st.peek();
+
+    }
+
+    public int getMin() {
+
+        return minStack.peek();
+
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
